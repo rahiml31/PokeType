9efffd0ec74182c98573cf920826ae3e74524c7a
Rahim <rahim.lalani31@gmail.com>
2018-04-08 17:37:39 -0400

    Added way to input Pokemon name rather than static. Also checks to see if in Dex.

--- src/PokeType/PokeMain.java
+++ src/PokeType/PokeMain.java
@@ -1,18 +1,33 @@
 package PokeType;
 
+import java.util.Scanner;
+
 public class PokeMain {
 
     /* For Nidoran♀, use Nidoran F, and for Nidoran♂, use Nidoran M */
 
     public static void main(String[] args) {
-       PokemonName pokemon=new PokemonName("Zapdos");
-       PokeDex dex=new PokeDex(pokemon);
-       TypeMain type=new TypeMain(pokemon);
-       dex.setPokedexNum();
-       type.findTypes();
-       System.out.println("Pokemon Name:\t"+pokemon.getPokename());
-       System.out.println("PokeDex Entry:\t"+dex.getDexnumber());
-       System.out.println("Pokemon Type: \t"+type.getTypelist());
+        System.out.println("Enter a Pokemon Name: ");
+        Scanner sc=new Scanner(System.in);
+        String pokemonName=sc.nextLine();
+        System.out.println();
+
+        PokemonName pokemon=new PokemonName(pokemonName);
+        PokeDex dex=new PokeDex(pokemon);
+        TypeMain type=new TypeMain(pokemon);
+
+        dex.setPokedexNum();
+        if(1<=dex.getDexnumber() && dex.getDexnumber()<=151 ) {
+            type.findTypes();
+
+            System.out.println("Pokemon Name:\t" + pokemon.getPokename());
+            System.out.println("PokeDex Entry:\t" + dex.getDexnumber());
+            System.out.println("Pokemon Type: \t" + type.getTypelist());
+        }
+        else {
+            System.out.println("Sorry the Pokemon chosen is not in the original 151.");
+        }
+
     }
 
 
